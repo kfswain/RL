eed7226639abbc7bfeeb4f19a322f6cc9241fdd7
Kellen Swain <kfswain@google.com>
2026-02-25 16:07:04 +0000

    dlasijd

--- nemo_rl/experience/rollouts.py
+++ nemo_rl/experience/rollouts.py
@@ -963,9 +963,9 @@ def run_async_multi_turn_rollout(
             # refresh endpoint metrics now to ensure we hold off on backpressure
             # we wait 10ms to not hog the thread/lock to allow metrics to refresh
             time.sleep(0.005)
-            metrics = policy_generation.get_vllm_logger_metrics()
-            for idx, ep in assigned_endpoints.items():
-                update_metrics(idx, ep, metrics)
+            # metrics = policy_generation.get_vllm_logger_metrics()
+            # for idx, ep in assigned_endpoints.items():
+            #     update_metrics(idx, ep, metrics)
 
 
             
